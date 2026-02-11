bec31208ec159c68b16ccbd87a2037ad2623fdc3
bayuwijanarko07 <bayuwijanarko07@gmail.com>
2026-02-11 12:01:11 +0700

    chore: config Internationalization

--- app/app.vue
+++ app/app.vue
@@ -1,30 +1,39 @@
 <script setup>
-useHead({
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-  ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
-  htmlAttrs: {
-    lang: 'en'
-  }
-})
+  import * as locales from '@nuxt/ui/locale'
+  const colorMode = useColorMode()
+  const { locale } = useI18n()
+
+  useHead({
+     meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'theme-color',
+        content: () =>
+          colorMode.value === 'dark' ? '#1b1718' : '#ffffff'
+      }
+    ],
+    link: [
+      { rel: 'icon', href: '/favicon.ico' }
+    ],
+    htmlAttrs: {
+      lang: () => locale.value
+    },
+  })
 
 
-useSeoMeta({
-  title: 'Bayu Wijanarko',
-  description: 'Personal website dan portfolio Bayu Wijanarko',
-  // ogTitle: title,
-  // ogDescription: description,
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterCard: 'summary_large_image'
-})
+  useSeoMeta({
+    title: 'Bayu Wijanarko',
+    description: 'Personal website dan portfolio Bayu Wijanarko',
+    ogTitle: 'Personal website dan portfolio Bayu Wijanarko',
+    ogDescription: 'Personal website dan portfolio Bayu Wijanarko',
+    twitterCard: 'summary_large_image'
+  })
+
 </script>
 
 <template>
-  <UApp>
+  <UApp :locale="locales[locale]" :color-mode="colorMode.value">
     <NuxtLoadingIndicator color="#3b82f6" :throttle="100"/>
     <UMain>
       <NuxtPage />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/eslint',
     '@vueuse/motion',
+    '@nuxtjs/i18n',
     '@nuxt/ui'
   ],
 
@@ -11,6 +12,32 @@ export default defineNuxtConfig({
   },
 
   css: ['~/assets/css/main.css'],
+
+  i18n: {
+    locales: [
+      {
+        code: 'id',
+        iso: 'id-ID',
+        name: 'Bahasa Indonesia',
+        file: 'id.json'
+      },
+      {
+        code: 'en',
+        iso: 'en-US',
+        name: 'English',
+        file: 'en.json'
+      }
+    ],
+    langDir: 'locales',
+    defaultLocale: 'id',
+    strategy: 'no_prefix',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root',
+      alwaysRedirect: false
+    },
+  },
 
   routeRules: {
     '/': { prerender: true }
